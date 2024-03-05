2d8ded2111e9e8da4221a9fe88d9928af68be8cf
Stephen Seo <seo.disparate@gmail.com>
2024-03-05 17:03:33 +0900

    Move `ffi` module to separate file

---
+++ src/ffi.rs
@@ -0,0 +1,14 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+include!(concat!(env!("OUT_DIR"), "/glfw_vk_bindings.rs"));
+
+pub fn VK_MAKE_VERSION(major: u32, minor: u32, patch: u32) -> u32 {
+    (major << 22) | (minor << 12) | patch
+}
+
+pub fn VK_MAKE_API_VERSION(variant: u32, major: u32, minor: u32, patch: u32) -> u32 {
+    (variant << 29) | (major << 22) | (minor << 12) | patch
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,4 @@
-mod ffi {
-    #![allow(non_upper_case_globals)]
-    #![allow(non_camel_case_types)]
-    #![allow(non_snake_case)]
-    #![allow(unused_imports)]
-    #![allow(dead_code)]
-    include!(concat!(env!("OUT_DIR"), "/glfw_vk_bindings.rs"));
-
-    pub fn VK_MAKE_VERSION(major: u32, minor: u32, patch: u32) -> u32 {
-        (major << 22) | (minor << 12) | patch
-    }
-
-    pub fn VK_MAKE_API_VERSION(variant: u32, major: u32, minor: u32, patch: u32) -> u32 {
-        (variant << 29) | (major << 22) | (minor << 12) | patch
-    }
-}
+mod ffi;
 
 use std::collections::HashSet;
 use std::ffi::{CStr, CString};
